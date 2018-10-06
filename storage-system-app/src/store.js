a6f42d75e8a688be0a991e7985c32baa78e94688
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-06 16:31:46 +0200

    created a redux store

---
+++ storage-system-app/src/store.js
@@ -0,0 +1,6 @@
+import { createStore, compose } from 'redux';
+import reducer from './reducers';
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+export default createStore(reducer, enhancers)
