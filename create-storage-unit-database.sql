21d3d85c6e4d1bdff2f978d5979c6b156867c312
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-06 14:46:55 +0200

    fixed spelling errors.

--- create-storage-unit-database.sql
+++ create-storage-unit-database.sql
@@ -1,50 +1,53 @@
 CREATE TABLE IF NOT EXISTS Business (
     id serial PRIMARY KEY,
-    name varchar(225) NOT NULL,
-    contact_name varchar(255) NOT NULL,
-    contact_Number varchar(255) NOT NULL,
-    contact_email varchar(255) NOT NULL,
+    name varchar(100) NOT NULL,
+    contact_name varchar(100) NOT NULL,
+    contact_Number varchar(100) NOT NULL,
+    contact_email varchar(200) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
 );CREATE TABLE IF NOT EXISTS Locations (
     id serial PRIMARY KEY,
-    address varchar(225) NOT NULL,
+    country varchar(150) NOT NULL,
+    province varchar(150) NOT NULL,
+    city varchar(150) NOT NULL,
+    address varchar(100) NOT NULL,
     business_id INT REFERENCES Business(id) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
 );CREATE TABLE IF NOT EXISTS Blocks (
     id serial PRIMARY KEY,
-    name varchar(225) NOT NULL,
+    name varchar(100) NOT NULL,
     locations_id INT REFERENCES Locations(id) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
-);CREATE TABLE IF NOT EXISTS Unite_types (
+);CREATE TABLE IF NOT EXISTS Unit_types (
     id serial PRIMARY KEY,
-    name varchar(225) NOT NULL,
+    name varchar(100) NOT NULL,
     length INT NOT NULL,
     width INT NOT NULL,
     height INT NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
-);CREATE TABLE IF NOT EXISTS Unites (
+);CREATE TABLE IF NOT EXISTS Units (
     id serial PRIMARY KEY,
-    name varchar(225) NOT NULL,
+    name varchar(100) NOT NULL,
     blocks_id INT REFERENCES Business(id) NOT NULL,
-    unite_type_id INT REFERENCES Unite_types(id) NOT NULL,
+    Unit_type_id INT REFERENCES Unit_types(id) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
 );CREATE TABLE IF NOT EXISTS clients(
     id serial PRIMARY KEY,
-    first_name varchar(225) NOT NULL,
-    last_name varchar(225) NOT NULL,
-    email varchar(225) NOT NULL,
-    telephone varchar(225) NOT NULL,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    email varchar(200) NOT NULL,
+    telephone varchar(100) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
 );CREATE TABLE IF NOT EXISTS used_storages(
     id serial PRIMARY KEY,
     client_id INT REFERENCES clients(id) NOT NULL,
-    unite_id INT REFERENCES Unites(id) NOT NULL UNIQUE,
+    Unit_id INT REFERENCES Units(id) NOT NULL UNIQUE,
     business_id INT REFERENCES Business(id) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
@@ -67,7 +70,7 @@ INSERT INTO
 VALUES
     ('section-B', 2)
 INSERT INTO
-    unite_types(
+    Unit_types(
         name,
         length,
         width,
@@ -76,10 +79,10 @@ INSERT INTO
 VALUES
     ('warehouse', 120, 180, 80)
 INSERT INTO
-    unites(
+    Units(
         name,
         blocks_id,
-        unite_type_id
+        Unit_type_id
     )
 VALUES
     ('A3', 1, 1)
@@ -95,7 +98,7 @@ VALUES
 INSERT INTO
     used_storages(
         client_id,
-        unite_id,
+        Unit_id,
         business_id
     )
 VALUES
@@ -105,25 +108,25 @@ SELECT
 FROM
     blocks
 SELECT
-    unites.name,
-    unites.blocks_id,
-    unites.unite_type_id
+    Units.name,
+    Units.blocks_id,
+    Units.Unit_type_id
 FROM
     business
     INNER JOIN locations on business.id = locations.business_id
     INNER JOIN blocks on locations.id = blocks.locations_id
-    INNER JOIN unites on blocks.id = unites.blocks_id
+    INNER JOIN Units on blocks.id = Units.blocks_id
 WHERE
     business.name = 'Mzansi';
 SELECT
-    unites.name,
-    unites.blocks_id,
-    unites.unite_type_id
+    Units.name,
+    Units.blocks_id,
+    Units.Unit_type_id
 FROM
-    unite_types
-    INNER JOIN unites on unite_types.id = unites.unite_type_id
+    Unit_types
+    INNER JOIN Units on Unit_types.id = Units.Unit_type_id
 WHERE
-    unite_types.name = 'garage'
+    Unit_types.name = 'garage'
 SELECT
     business.name,
     business.contact_name,
@@ -134,14 +137,14 @@ FROM
     business
     INNER JOIN locations on business.id = locations.business_id;
 SELECT
-    unites.name,
-    unites.blocks_id,
-    unites.unite_type_id,
-    unite_types.name,
-    unite_types.lenght,
-    unite_types.width
+    Units.name,
+    Units.blocks_id,
+    Units.Unit_type_id,
+    Unit_types.name,
+    Unit_types.lenght,
+    Unit_types.width
 FROM
-    unite_types
-    INNER JOIN unites on unite_types.id = unites.unite_type_id
+    Unit_types
+    INNER JOIN Units on Unit_types.id = Units.Unit_type_id
 WHERE
-    unite_types.width > 50;
+    Unit_types.width > 50;
