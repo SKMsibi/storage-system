e24522edb0fc341d0e7896d78223a4b0a936c5b1
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-08 13:14:19 +0200

    added redux and react router to the app.

--- storage-system-app/src/index.js
+++ storage-system-app/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import store from './store';
+import RegisterBlocks from './components/register-blocks';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './index.css';
+import store from './store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
-    </Provider>
-    , document.getElementById('root'));
+        <Router>
+            <div>
+                <Route path="/" component={App} />
+                <Route path="/specifyBlocks" component={RegisterBlocks} />
+            </div>
+        </Router>
+    </Provider >, document.getElementById('root'));
