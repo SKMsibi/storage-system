38c7b90103ea8c5c762aecbe908e418eaa3a03c1
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-08 13:16:10 +0200

    removed the  submit funciton.

--- storage-system-app/src/App.js
+++ storage-system-app/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import BusinessForm from './components/redux-form';
+import BusinessForm from './components/register-business-form';
 import './App.css';
 
 class App extends Component {
-  submit(data) {
-    console.log("data", data);
-  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <BusinessForm handleSubmit={this.submit.bind(this)} />
+          <BusinessForm />
         </header>
       </div>
     );
