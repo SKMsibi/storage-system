b924227ce8f0f89faf6973b85fea37b96a4f5785
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-08 15:10:04 +0200

    added a route for the business registration

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+const cors = require("cors");
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/businessData', function (req, res, next) {
-  // res.send(req.query)
+app.post('/businessData', function (req, res) {
+  console.log("to be added to the database", req.body);
+  res.status(201).end();
 });
 
 app.listen(3003, function () {
