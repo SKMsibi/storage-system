205b494c0bddbdfb793c5a6327794d0196de5c57
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-08 15:09:38 +0200

    added a connect method and a function that sends the form data to the api side.

--- storage-system-app/src/components/register-business-form.js
+++ storage-system-app/src/components/register-business-form.js
@@ -1,61 +1,75 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import axios from "axios";
 import { Redirect } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
 
-class BusinessForm extends Component {
+export class BusinessForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            shouldRedirect: false
+            shouldRedirect: false,
+            error: false,
+            errorMessage: ""
         }
     }
     componentDidMount = () => {
         this.setState({ shouldRedirect: false })
     }
 
+    async registerBusiness() {
+        var sendingData = await axios.post('http://localhost:3003/businessData', this.props.businessForm.RegisterBusiness.values);
+        if (sendingData.status === 201) {
+            this.setState({ shouldRedirect: true })
+        } else {
+            this.setState({ error: true, errorMessage: sendingData.data })
+        }
+    }
     render() {
         return (
             <div>
                 {!this.state.shouldRedirect && (
-                    <form action="http://localhost:3003/testing" method="get">
-                        <div className="business-info">
-                            <div className="form-row">
-                                <label htmlFor="firstName">Business Name</label>
-                                <Field name="businessName" component="input" type="text" />
+                    <div>
+                        <form>
+                            <div className="business-info">
+                                <div className="form-row">
+                                    <label htmlFor="firstName">Business Name</label>
+                                    <Field name="businessName" component="input" type="text" />
+                                </div>
+                                <div className="form-row">
+                                    <label htmlFor="lastName">Contact Name</label>
+                                    <Field name="contactName" component="input" type="text" />
+                                </div>
+                                <div className="form-row">
+                                    <label htmlFor="telephone">Telephone</label>
+                                    <Field name="telephone" component="input" type="tel" />
+                                </div>
+                                <div className="form-row">
+                                    <label htmlFor="email">Email</label>
+                                    <Field name="email" component="input" type="email" />
+                                </div>
                             </div>
-                            <div className="form-row">
-                                <label htmlFor="lastName">Contact Name</label>
-                                <Field name="contactName" component="input" type="text" />
+                            <div className="business-address">
+                                <div className="form-row">
+                                    <label htmlFor="country">Country</label>
+                                    <Field name="country" component="input" type="text" />
+                                </div>
+                                <div className="form-row">
+                                    <label htmlFor="address1">address1</label>
+                                    <Field name="address1" component="input" type="text" />
+                                </div>
+                                <div className="form-row">
+                                    <label htmlFor="address2">Address2</label>
+                                    <Field name="address2" component="input" type="text" />
+                                </div>
+                                <div className="form-row">
+                                    <label htmlFor="address3">Address3</label>
+                                    <Field name="address3" component="input" type="text" />
+                                </div>
                             </div>
-                            <div className="form-row">
-                                <label htmlFor="telephone">Telephone</label>
-                                <Field name="telephone" component="input" type="tel" />
-                            </div>
-                            <div className="form-row">
-                                <label htmlFor="email">Email</label>
-                                <Field name="email" component="input" type="email" />
-                            </div>
-                        </div>
-                        <div className="business-address">
-                            <div className="form-row">
-                                <label htmlFor="country">Country</label>
-                                <Field name="country" component="input" type="text" />
-                            </div>
-                            <div className="form-row">
-                                <label htmlFor="address1">address1</label>
-                                <Field name="address1" component="input" type="text" />
-                            </div>
-                            <div className="form-row">
-                                <label htmlFor="address2">Address2</label>
-                                <Field name="address2" component="input" type="text" />
-                            </div>
-                            <div className="form-row">
-                                <label htmlFor="address3">Address3</label>
-                                <Field name="address3" component="input" type="text" />
-                            </div>
-                        </div>
-                        <button type="submit" onClick={() => this.setState({ shouldRedirect: true })}>Next</button>
-                    </form>
+                        </form>
+                        <button onClick={() => this.registerBusiness()}>Next</button>
+                    </div>
                 )}
                 {this.state.shouldRedirect && (
                     <Redirect to='/specifyBlocks' />
@@ -67,4 +81,10 @@ class BusinessForm extends Component {
 BusinessForm = reduxForm({
     form: 'RegisterBusiness'
 })(BusinessForm)
-export default BusinessForm;
+
+const mapStateToProps = state => {
+    return { businessForm: state.form };
+}
+const mapDispatchToProps = dispatch => ({
+})
+export default connect(mapStateToProps, mapDispatchToProps)(BusinessForm);
