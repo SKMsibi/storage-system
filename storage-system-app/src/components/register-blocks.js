03fcaf8278936b3392ecfa6bb371819022c20fd7
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-09 09:23:24 +0200

    created state for the register block component. added functions to help with the block registration and called the blockForm component

--- storage-system-app/src/components/register-blocks.js
+++ storage-system-app/src/components/register-blocks.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import BlockForm from './insert-block-form';
 import '../App.css';
 
 export class RegisterBlocks extends Component {
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            numberOfBlocks: 1,
+            blocks: [],
+            showBlocksInsert: false
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.createBlockForms = this.createBlockForms.bind(this);
+    }
+    handleChange(event) {
+        this.setState({ numberOfBlocks: event.target.value, showBlocksInsert: false });
+    }
+    createBlockForms() {
+        var arrTemp = [];
+        for (let index = 1; index <= this.state.numberOfBlocks; index++) {
+            arrTemp.push(index);
+        }
+        this.setState({ blocks: arrTemp, showBlocksInsert: true })
+    }
     render() {
         return (
             <div className="register-blocks">
                 <header className="App-header">
                     <label>How many blocks are you adding?</label>
-                    <input type="numb" name="numberOfBlocks" />
+                    <input type="number" min="1" name="numberOfBlocks" value={this.state.numberOfBlocks} onChange={this.handleChange} />
+                    <button onClick={this.createBlockForms}>Go</button>
+                    {this.state.blocks.map(item => {
+                        return <BlockForm number={item} key={item} />
+                    })}
+                    {this.state.showBlocksInsert && (
+                        <button>Insert Blocks</button>
+                    )}
                 </header>
             </div>
         );
