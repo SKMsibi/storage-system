b90417caee6927ffc978b3f9d8cb07dec4487d4f
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-12 10:16:44 +0200

    changed the locations column names.

--- create-storage-unit-database.sql
+++ create-storage-unit-database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS business (
     id serial PRIMARY KEY,
-    name varchar(100) NOT NULL,
+    name varchar(100) NOT NULL UNIQUE,
     contact_name varchar(100) NOT NULL,
     contact_Number varchar(100) NOT NULL,
     contact_email varchar(200) NOT NULL,
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS business (
 );CREATE TABLE IF NOT EXISTS locations (
     id serial PRIMARY KEY,
     country varchar(150) NOT NULL,
-    province varchar(150) NOT NULL,
-    city varchar(150) NOT NULL,
-    address varchar(100) NOT NULL,
+    address1 varchar(150) NOT NULL,
+    address2 varchar(150) NOT NULL,
+    address3 varchar(100) NOT NULL,
     business_id INT REFERENCES business(id) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS business (
     id serial PRIMARY KEY,
     first_name varchar(100) NOT NULL,
     last_name varchar(100) NOT NULL,
-    email varchar(200) NOT NULL,
+    email varchar(200) NOT NULL UNIQUE,
     telephone varchar(100) NOT NULL,
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
@@ -51,99 +51,98 @@ CREATE TABLE IF NOT EXISTS business (
     created_at timestamp NOT NULL DEFAULT NOW() NOT NULL,
     updated_at timestamp NOT NULL DEFAULT NOW() NOT NULL
 );
--- INSERT INTO
---     business(
---         name,
---         contact_name,
---         contact_number,
---         contact_email
---     )
--- VALUES
---     ('TCG', 'Theo', '071659813', 'Theo@tcg.com');
--- INSERT INTO
---     locations(address, business_id)
--- VALUES
---     ('Gauteng Johannesburg Fourways riversands', 2)
--- INSERT INTO
---     blocks(name, locations_id)
--- VALUES
---     ('section-B', 2)
--- INSERT INTO
---     Unit_types(
---         name,
---         length,
---         width,
---         height
---     )
--- VALUES
---     ('warehouse', 120, 180, 80)
--- INSERT INTO
---     Units(
---         name,
---         blocks_id,
---         Unit_type_id
---     )
--- VALUES
---     ('A3', 1, 1)
--- INSERT INTO
---     clients(
---         first_name,
---         last_name,
---         email,
---         telephone
---     )
--- VALUES
---     ('Sabelo', 'Msibi', 'skm@gmail.com', '0715585598')
--- INSERT INTO
---     used_storages(
---         client_id,
---         Unit_id,
---         business_id
---     )
--- VALUES
---     (2, 17, 2)
--- SELECT
---     *
--- FROM
---     blocks
--- SELECT
---     Units.name,
---     Units.blocks_id,
---     Units.Unit_type_id
--- FROM
---     business
---     INNER JOIN locations on business.id = locations.business_id
---     INNER JOIN blocks on locations.id = blocks.locations_id
---     INNER JOIN Units on blocks.id = Units.blocks_id
--- WHERE
---     business.name = 'Mzansi';
--- SELECT
---     Units.name,
---     Units.blocks_id,
---     Units.Unit_type_id
--- FROM
---     Unit_types
---     INNER JOIN Units on Unit_types.id = Units.Unit_type_id
--- WHERE
---     Unit_types.name = 'garage'
--- SELECT
---     business.name,
---     business.contact_name,
---     business.contact_number,
---     business.contact_email,
---     locations.address
--- FROM
---     business
---     INNER JOIN locations on business.id = locations.business_id;
--- SELECT
---     Units.name,
---     Units.blocks_id,
---     Units.Unit_type_id,
---     Unit_types.name,
---     Unit_types.lenght,
---     Unit_types.width
--- FROM
---     Unit_types
---     INNER JOIN Units on Unit_types.id = Units.Unit_type_id
--- WHERE
---     Unit_types.width > 50;
+INSERT INTO
+    business(
+        name,
+        contact_name,
+        contact_number,
+        contact_email
+    )
+VALUES
+    ('TCG', 'Theo', '071659813', 'Theo@tcg.com');
+INSERT INTO
+    locations(country, address1,address2, address3, business_id)
+VALUES
+    ('South Africa','alexander township','corner London road sixth evanue','boikutsong flats block8 no9', 1);
+INSERT INTO
+    blocks(name, locations_id)
+VALUES
+    ('section-B', 2);
+INSERT INTO
+    Unit_types(
+        name,
+        length,
+        width,
+        height
+    )
+VALUES
+    ('warehouse', 120, 180, 80);
+INSERT INTO
+    Units(
+        name,
+        blocks_id,
+        Unit_type_id
+    )
+VALUES
+    ('A3', 1, 1);
+INSERT INTO
+    clients(
+        first_name,
+        last_name,
+        email,
+        telephone
+    )
+VALUES
+    ('Sabelo', 'Msibi', 'skm@gmail.com', '0715585598');
+INSERT INTO
+    used_storages(
+        client_id,
+        Unit_id,
+        business_id
+    )
+VALUES
+    (2, 17, 2);-- SELECT
+    --     *
+    -- FROM
+    --     blocks
+    -- SELECT
+    --     Units.name,
+    --     Units.blocks_id,
+    --     Units.Unit_type_id
+    -- FROM
+    --     business
+    --     INNER JOIN locations on business.id = locations.business_id
+    --     INNER JOIN blocks on locations.id = blocks.locations_id
+    --     INNER JOIN Units on blocks.id = Units.blocks_id
+    -- WHERE
+    --     business.name = 'Mzansi';
+    -- SELECT
+    --     Units.name,
+    --     Units.blocks_id,
+    --     Units.Unit_type_id
+    -- FROM
+    --     Unit_types
+    --     INNER JOIN Units on Unit_types.id = Units.Unit_type_id
+    -- WHERE
+    --     Unit_types.name = 'garage'
+    -- SELECT
+    --     business.name,
+    --     business.contact_name,
+    --     business.contact_number,
+    --     business.contact_email,
+    --     locations.address
+    -- FROM
+    --     business
+    --     INNER JOIN locations on business.id = locations.business_id;
+    -- SELECT
+    --     Units.name,
+    --     Units.blocks_id,
+    --     Units.Unit_type_id,
+    --     Unit_types.name,
+    --     Unit_types.lenght,
+    --     Unit_types.width
+    -- FROM
+    --     Unit_types
+    --     INNER JOIN Units on Unit_types.id = Units.Unit_type_id
+    -- WHERE
+    --     Unit_types.width > 50;
