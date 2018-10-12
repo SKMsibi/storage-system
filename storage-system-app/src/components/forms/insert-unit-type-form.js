52a23831467525d1961d42f6ea43ebcf28da378a
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-12 10:21:21 +0200

    created the unit type form component.

---
+++ storage-system-app/src/components/forms/insert-unit-type-form.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
+
+export class UnitTypeForm extends Component {
+    render() {
+        return (
+            <form>
+                <div className="business-info">
+                    <div className="form-row-unit">
+                        <label htmlFor='unitName'>Unit Name</label>
+                        <Field name='unitName' component="input" type="text" />
+                        <label htmlFor='blockName'>Block Name</label>
+                        <Field name='blockName' component="input" type="text" />
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
+UnitTypeForm = reduxForm({
+    form: 'InsertUniteTypeForm'
+})(UnitTypeForm)
+
+const mapStateToProps = state => {
+    return { UnitTypeForm: state.form };
+}
+export default connect(mapStateToProps, null)(UnitTypeForm);
