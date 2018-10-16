82fec176920e4825d6bccd0487bc0aa290fdc3b7
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-16 16:25:46 +0200

    added properties to the insertLocationForm. added the redirect class.

--- storage-system-app/src/components/insert-location.js
+++ storage-system-app/src/components/insert-location.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import InsertLocationForm from './forms/insert-location-form'
 import axios from 'axios';
 
@@ -9,7 +10,9 @@ export class InsertLocation extends Component {
         super();
         this.state = {
             showForm: false,
-            allBusiness: []
+            allBusiness: [],
+            selectedBusiness: '',
+            shouldRedirect: false
         }
     }
     async componentDidMount() {
@@ -22,14 +25,17 @@ export class InsertLocation extends Component {
                 <div>
                     <h3>Business Address</h3>
                     <p>Please select the business you want to insert location for.</p>
-                    <select ref="select" onChange={() => { if (this.refs.select.value === "Select Business") { this.setState({ showForm: false }) } else { this.setState({ showForm: true }) } }}>
+                    <select ref="select" onChange={() => { if (this.refs.select.value === "Select Business") { this.setState({ showForm: false }) } else { this.setState({ showForm: true, selectedBusiness: this.refs.select.value }) } }}>
                         <option value="Select Business">Select Business</option>
                         {this.state.allBusiness.map(singleBusiness => {
                             return <option key={this.state.allBusiness.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
                         })}
                     </select>
                     {this.state.showForm && (
-                        <InsertLocationForm />
+                        <InsertLocationForm selectedBusiness={this.state.selectedBusiness} redirect={() => this.setState({ shouldRedirect: true })} />
+                    )}
+                    {this.state.shouldRedirect && (
+                        <Redirect to='/insertBlocks' />
                     )}
                     <div>
                         <Link to="/"><button>back</button></Link>
