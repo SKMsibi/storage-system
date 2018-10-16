6dfef42a0d55fa7ff06e5d0f51c911f262c3fe81
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-16 14:19:18 +0200

    created a new component for inserting a business location.

---
+++ storage-system-app/src/components/insert-location.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+import InsertLocationForm from './forms/insert-location-form'
+import axios from 'axios';
+
+export class InsertLocation extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showForm: false,
+            allBusiness: []
+        }
+    }
+    async componentDidMount() {
+        var allTheBusiness = await axios.get('http://localhost:3003/business');
+        this.setState({ allBusiness: allTheBusiness.data })
+    }
+    render() {
+        return (
+            <header className="App-header">
+                <div>
+                    <h3>Business Address</h3>
+                    <p>Please select the business you want to insert location for.</p>
+                    <select ref="select" onChange={() => { if (this.refs.select.value === "Select Business") { this.setState({ showForm: false }) } else { this.setState({ showForm: true }) } }}>
+                        <option value="Select Business">Select Business</option>
+                        {this.state.allBusiness.map(singleBusiness => {
+                            return <option key={this.state.allBusiness.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
+                        })}
+                    </select>
+                    {this.state.showForm && (
+                        <InsertLocationForm />
+                    )}
+                    <div>
+                        <Link to="/"><button>back</button></Link>
+                    </div>
+                </div >
+            </header>
+        )
+    }
+}
+const mapStateToProps = state => {
+    return { businessForm: state.form };
+}
+export default connect(mapStateToProps, null)(InsertLocation);
