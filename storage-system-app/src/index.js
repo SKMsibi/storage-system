84b43226df835df6d4943e56ab851fdaa013e95e
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-16 14:18:56 +0200

    added the location route.

--- storage-system-app/src/index.js
+++ storage-system-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import RegisterBlocks from './components/register-blocks';
 import InsertUniteType from './components/insert-unit-type';
+import InsertLocation from './components/insert-location'
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
@@ -15,6 +16,7 @@ ReactDOM.render(
                 <Route exact path="/" component={App} />
                 <Route exact path="/insertBlocks" component={RegisterBlocks} />
                 <Route exact path="/insertUnitType" component={InsertUniteType} />
+                <Route exact path="/insertLocation" component={InsertLocation} />
             </div>
         </Router>
     </Provider >, document.getElementById('root'));
