2bb977187f25b76bd9223ed926e927d92cea2133
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-16 16:24:37 +0200

    inserted a post route for adding business location to the database.

--- app.js
+++ app.js
@@ -25,12 +25,21 @@ app.get('/business', async function (req, res) {
   } else {
     res.status(500).end();
   }
+});
+app.post('/businessLocation', async function (req, res) {
+  try {
+    const businessId = await client.query(`SELECT id FROM business WHERE name = '${req.body.businessName}';`);
+    await client.query(`INSERT INTO locations(country, address1,address2, address3, business_id) VALUES ( ${req.body.country ? `'${req.body.country}'` : null}, ${req.body.address1 ? `'${req.body.address1}'` : null}, ${req.body.address2 ? `'${req.body.address2}'` : null}, ${req.body.address3 ? `'${req.body.address3}'` : null}, ${businessId.rows[0].id > 0 ? businessId.rows[0].id : null} )`);
+    res.status(201).end();
+  } catch (error) {
+    res.status(500).send("sorry cant register business address : " + `${error}`).end();
+  }
 })
 app.post('/submitBlocks', async function (req, res) {
   await client.query(`INSERT INTO blocks(name, contact_name, contact_number, contact_email) VALUES ( ${req.body.businessName ? `'${req.body.businessName}'` : null}, ${req.body.contactName ? `'${req.body.contactName}'` : null}, ${req.body.telephone ? `'${req.body.telephone}'` : null}, ${req.body.email ? `'${req.body.email}'` : null})`);
   console.log("blocks to be added to the database", req.body);
   res.status(201).end();
-})
+});
 
 app.listen(3003, function () {
   console.log('web server listening on port 3003')
