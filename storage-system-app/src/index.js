88e7550b18c3b666b8966b6cd41634e6e832a67c
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-18 10:07:16 +0200

    added a displayBusinessData route.

--- storage-system-app/src/index.js
+++ storage-system-app/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import RegisterBlocks from './components/register-blocks';
-import InsertUniteType from './components/insert-unit-type';
-import InsertLocation from './components/insert-location'
+import InsertUnite from './components/insert-unit-type';
+import InsertLocation from './components/insert-location';
+import viewInfo from "./components/view-info";
+import navbar from './components/navbar'
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
@@ -13,10 +15,12 @@ ReactDOM.render(
     <Provider store={store}>
         <Router>
             <div>
+                <Route path="/" component={navbar} />
                 <Route exact path="/" component={App} />
                 <Route exact path="/insertBlocks" component={RegisterBlocks} />
-                <Route exact path="/insertUnitType" component={InsertUniteType} />
+                <Route exact path="/insertUnitType" component={InsertUnite} />
                 <Route exact path="/insertLocation" component={InsertLocation} />
+                <Route exact path="/displayBusinessData" component={viewInfo} />
             </div>
         </Router>
     </Provider >, document.getElementById('root'));
