9d95066b27e18da5bdd19fa2dbcf27860e1835ff
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-22 14:12:28 +0200

    added a feature that endables the user to add blocks to the database.

--- storage-system-app/src/components/register-blocks.js
+++ storage-system-app/src/components/register-blocks.js
@@ -9,13 +9,20 @@ export class RegisterBlocks extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            allBusiness: [],
+            showForm: false,
             numberOfBlocks: 1,
             blocks: [],
-            showBlocksInsert: false
+            showBlocksInsert: false,
+            selectedBusiness: ""
         }
         this.handleChange = this.handleChange.bind(this);
         this.createBlockForms = this.createBlockForms.bind(this);
         this.submitBlocks = this.submitBlocks.bind(this);
+    }
+    async componentDidMount() {
+        var allTheBusiness = await axios.get('http://localhost:3003/businesses');
+        this.setState({ allBusiness: allTheBusiness.data });
     }
     handleChange(event) {
         this.setState({ numberOfBlocks: event.target.value, showBlocksInsert: false });
@@ -28,29 +35,47 @@ export class RegisterBlocks extends Component {
         this.setState({ blocks: arrTemp, showBlocksInsert: true })
     }
     async submitBlocks() {
-        var sendingBlocks = await axios.post('http://localhost:3003/submitBlocks', this.props.blocksData.InsertBlockForm.values);
+        var sendingBlocks = await axios.post('http://localhost:3003/submitBlocks', { formValues: this.props.blocksData.InsertBlockForm.values, businessName: this.state.selectedBusiness });
         if (sendingBlocks.status === 201) {
             this.setState({ shouldRedirect: true })
         } else {
             this.setState({ error: true, errorMessage: sendingBlocks.data })
         }
     }
+    handleBusinessSelection() {
+        if (this.refs.select.value === "Select Business") {
+            this.setState({ showForm: false })
+        } else {
+            this.setState({ showForm: true, selectedBusiness: this.refs.select.value })
+        }
+    }
     render() {
         return (
             <div className="register-blocks">
                 <div className="App-container">
-                    <label>How many blocks are you adding?</label>
-                    <input type="number" min="1" name="numberOfBlocks" value={this.state.numberOfBlocks} onChange={this.handleChange} />
-                    <button onClick={this.createBlockForms}>Go</button>
-                    {this.state.blocks.map(item => {
-                        return <BlockForm number={item} key={item} />
-                    })}
-                    {this.state.showBlocksInsert && (
-                        <Link to="/insertUnitType"><button onClick={this.submitBlocks}>Insert Blocks</button></Link>
+                    <label>Select the business you want to insert Blocks for.</label>
+                    <select ref="select" onChange={() => this.handleBusinessSelection()}>
+                        <option value="Select Business">Select Business</option>
+                        {this.state.allBusiness.map(singleBusiness => {
+                            return <option key={this.state.allBusiness.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
+                        })}
+                    </select>
+                    {this.state.showForm && (
+                        <div className="block-form">
+                            <label>How many blocks are you adding?</label><br />
+                            <input type="number" min="1" name="numberOfBlocks" value={this.state.numberOfBlocks} onChange={this.handleChange} />
+                            <button ref="addToDatabase" onClick={this.createBlockForms}>Go</button>
+                            {this.state.blocks.map(item => {
+                                return <BlockForm number={item} key={item} />
+                            })}
+                            {this.state.showBlocksInsert && (
+                                <Link to="/insertUnitType"><button onClick={this.submitBlocks}>Insert Blocks</button></Link>
+                            )}
+                        </div>
                     )}
                     <Link to="/"><button>back</button></Link>
                 </div>
-            </div>
+            </div >
         );
     }
 }
