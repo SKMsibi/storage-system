5d65efc9b15e018b8a0c4cda8324297692ffa185
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-22 14:12:01 +0200

    changed the route name of the view component.

--- storage-system-app/src/components/navbar.js
+++ storage-system-app/src/components/navbar.js
@@ -10,7 +10,7 @@ export default class navbar extends Component {
                 <Link to="/insertLocation"><button className="navigation-button">Business location</button></Link>
                 <Link to="/insertBlocks"><button className="navigation-button">Insert Blocks</button></Link>
                 <Link to="/insertUnitType"><button className="navigation-button">Insert unites</button></Link>
-                <Link to="/displayBusinessData"><button className="navigation-button">view Business</button></Link>
+                <Link to="/displayUnits"><button className="navigation-button">view Units</button></Link>
             </header>
         )
     }
