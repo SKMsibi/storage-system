80a8314829b2bde74dd15b71e57cb4161ce4af8b
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-24 13:07:03 +0200

    moved local state to redux store.

--- storage-system-app/src/components/insert-location.js
+++ storage-system-app/src/components/insert-location.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom'
-import { Redirect } from 'react-router-dom'
-import InsertLocationForm from './forms/insert-location-form'
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import InsertLocationForm from './forms/insert-location-form';
+import * as actions from '../redux/actions';
+import businesses from '../redux/thunks'
 import axios from 'axios';
 
 export class InsertLocation extends Component {
@@ -10,14 +12,12 @@ export class InsertLocation extends Component {
         super();
         this.state = {
             showForm: false,
-            allBusiness: [],
             selectedBusiness: '',
             shouldRedirect: false
         }
     }
     async componentDidMount() {
-        var allTheBusiness = await axios.get('http://localhost:3003/businesses');
-        this.setState({ allBusiness: allTheBusiness.data })
+        this.props.getBusinesses();
     }
     render() {
         return (
@@ -27,8 +27,8 @@ export class InsertLocation extends Component {
                     <p>Please select the business you want to insert location for.</p>
                     <select ref="select" onChange={() => { if (this.refs.select.value === "Select Business") { this.setState({ showForm: false }) } else { this.setState({ showForm: true, selectedBusiness: this.refs.select.value }) } }}>
                         <option value="Select Business">Select Business</option>
-                        {this.state.allBusiness.map(singleBusiness => {
-                            return <option key={this.state.allBusiness.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
+                        { this.props.businesses.allBusinesses.map(singleBusiness => {
+                            return <option key={this.props.businesses.allBusinesses.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
                         })}
                     </select>
                     {this.state.showForm && (
@@ -46,6 +46,20 @@ export class InsertLocation extends Component {
     }
 }
 const mapStateToProps = state => {
-    return { businessForm: state.form };
+    return {
+        state: state,
+        businessForm: state.form,
+        businesses: state.business
+    };
 }
-export default connect(mapStateToProps, null)(InsertLocation);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getBusinesses: () => {
+            dispatch(businesses())
+        },
+        setBusinessName: (name) => {
+            dispatch(actions.changeSelectedBusiness(name))
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(InsertLocation);
