9f629095cc90c7f36a8b87e28f7448b8ab67085c
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-24 13:12:56 +0200

    included the business reducer in the create reducer.

--- storage-system-app/src/redux/reducers/index.js
+++ storage-system-app/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { reducer as formReducer } from 'redux-form'
+import business from './business'
 import { combineReducers } from 'redux'
 
 export default combineReducers({
-    form: formReducer
+    form: formReducer,
+    business: business
 });
