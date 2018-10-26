7084e2b6c3e3889d1441efe925c23aeab3d20145
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-26 10:41:53 +0200

    added code to make the add blocks feature better.

--- storage-system-app/src/components/register-blocks.js
+++ storage-system-app/src/components/register-blocks.js
@@ -10,12 +10,15 @@ export class RegisterBlocks extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            allBusinessLocations: [],
             allBusiness: [],
             showForm: false,
             numberOfBlocks: 1,
             blocks: [],
             showBlocksInsert: false,
             selectedBusiness: "",
+            selectedLocation: "",
+            selectedLocationId: null,
             shouldRedirect: false
         }
         this.handleChange = this.handleChange.bind(this);
@@ -37,7 +40,7 @@ export class RegisterBlocks extends Component {
         this.setState({ blocks: arrTemp, showBlocksInsert: true })
     }
     async submitBlocks() {
-        var sendingBlocks = await axios.post('http://localhost:3003/submitBlocks', { formValues: this.props.blocksData.InsertBlockForm.values, businessName: this.state.selectedBusiness });
+        var sendingBlocks = await axios.post('http://localhost:3003/submitBlocks', { formValues: this.props.blocksData.InsertBlockForm.values, businessName: this.state.selectedBusiness, selectedLocation: this.state.selectedLocation });
         if (sendingBlocks.status === 201) {
             this.setState({ shouldRedirect: true })
         } else {
@@ -46,25 +49,48 @@ export class RegisterBlocks extends Component {
     }
     handleBusinessSelection() {
         if (this.refs.select.value === "Select Business") {
+            this.setState({ showLocationDropDown: false })
+        } else {
+            setTimeout(async () => {
+                var allBusinessLocations = await axios.get('http://localhost:3003/locationsForBusiness/' + this.refs.select.value);
+                this.setState({ showLocationDropDown: true, allBusinessLocations: allBusinessLocations.data })
+            }, 1000);
+        }
+    }
+    handleLocationSelection() {
+        if (this.refs.location.value === "Select location") {
             this.setState({ showForm: false })
         } else {
-            this.setState({ showForm: true, selectedBusiness: this.refs.select.value })
+            this.setState({ showForm: true, selectedLocation: this.refs.location.value })
         }
     }
     render() {
         return (
             <div className="register-blocks">
                 <div className="App-container">
-                    <label>Select the business you want to insert Blocks for.</label>
+                    <h4>Select the business you want to insert Blocks for.</h4>
                     <select ref="select" onChange={() => this.handleBusinessSelection()}>
                         <option value="Select Business">Select Business</option>
                         {this.state.allBusiness.map(singleBusiness => {
                             return <option key={this.state.allBusiness.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
                         })}
                     </select>
+                    {this.state.showLocationDropDown && (
+                        <h4>Select the location the Blocks.</h4>
+                    )}
+                    {this.state.showLocationDropDown && (
+                        <div>
+                            <select ref="location" onChange={() => this.handleLocationSelection()}>
+                                <option value="Select location">Select location</option>
+                                {this.state.allBusinessLocations.map(singleLocation => {
+                                    return <option key={this.state.allBusinessLocations.indexOf(singleLocation)} value={`${singleLocation.country},${singleLocation.address1},${singleLocation.address2},${singleLocation.address3}`}>{singleLocation.country}, {singleLocation.address1}, {singleLocation.address2}, {singleLocation.address3}</option>
+                                })}
+                            </select>
+                        </div>
+                    )}
                     {this.state.showForm && (
                         <div className="block-form">
-                            <label>How many blocks are you adding?</label><br />
+                            <h4>How many blocks are you adding?</h4><br />
                             <input type="number" min="1" name="numberOfBlocks" value={this.state.numberOfBlocks} onChange={this.handleChange} />
                             <button ref="addToDatabase" onClick={this.createBlockForms}>Go</button>
                             {this.state.blocks.map(item => {
