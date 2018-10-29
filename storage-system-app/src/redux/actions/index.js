7c5473b65d113f7695f4ad017adba33e8a526f02
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-29 11:10:55 +0200

    added a action creator.

--- storage-system-app/src/redux/actions/index.js
+++ storage-system-app/src/redux/actions/index.js
@@ -1,6 +1,9 @@
 export function getAllBusinesses(businesses) {
     return { type: "GET_BUSINESSES", value: businesses }
-}
+};
 export function changeSelectedBusiness(name) {
     return { type: "BUSINESSES_NAME", value: name }
+};
+export function removeError() {
+    return { type: "ERROR_NOT_PRESENT" };
 }
