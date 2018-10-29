aebeebae675e49fc4d4dab2890c11b8a09c00da6
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-29 11:11:38 +0200

    created a submit business thunk function.

--- storage-system-app/src/redux/thunks.js
+++ storage-system-app/src/redux/thunks.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 import * as actions from "./actions";
-export default function businesses() {
+export function getBusinesses() {
     return async (dispatch) => {
         var allTheBusiness = await axios.get('http://localhost:3003/businesses');
         if (allTheBusiness.data) {
             dispatch(actions.getAllBusinesses(allTheBusiness.data));
         }
     };
-}
+};
+export function submitBusiness(businessData) {
+    return async (dispatch) => {
+        try {
+            await axios.post('http://localhost:3003/businessData', businessData);
+            dispatch({ type: "SUBMIT" });
+            setTimeout(() => {
+                dispatch({ type: "COMPLETE_SUBMISSIONS" });
+            }, 1000);
+        } catch (error) {
+            dispatch({ type: "ERROR_PRESENT" });
+        }
+    };
+
+};
