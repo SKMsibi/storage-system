45755202b163c1a10b5f683ecdca0251c5a786cc
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-30 14:13:37 +0200

    added two cases and two properties in the block reducer

--- storage-system-app/src/redux/reducers/blocks.js
+++ storage-system-app/src/redux/reducers/blocks.js
@@ -1,11 +1,19 @@
 export default function blocks(state = {
     allBusinessWithLocation: [],
+    selectedBusiness: null,
+    selectedBusinessLocations: [],
     errorPresent: false
 }, action) {
     var newState = { ...state }
     switch (action.type) {
         case "SET_BUSINESSES":
             newState = { ...newState, allBusinessWithLocation: [...action.newValue] };
+            break;
+        case "CHANGE_SELECTED_BUSINESS":
+            newState = { ...newState, selectedBusiness: action.newValue };
+            break;
+        case "CHANGE_SELECTED_BUSINESS_LOCATIONS":
+            newState = { ...newState, selectedBusinessLocations: action.newValue };
             break;
         case "ERROR_CREATED":
             newState = { ...newState, errorPresent: true };
