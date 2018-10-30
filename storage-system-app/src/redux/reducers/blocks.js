cd0b13a643afa8f0f18a19ae9fe59027effb79f6
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-10-30 11:40:07 +0200

    added a property and a case for errors.

--- storage-system-app/src/redux/reducers/blocks.js
+++ storage-system-app/src/redux/reducers/blocks.js
@@ -1,12 +1,17 @@
 export default function blocks(state = {
-    allBusinessWithLocation: []
+    allBusinessWithLocation: [],
+    errorPresent: false
 }, action) {
     var newState = { ...state }
     switch (action.type) {
-        case "SET_LOCATIONS":
+        case "SET_BUSINESSES":
             newState = { ...newState, allBusinessWithLocation: [...action.newValue] };
             break;
+        case "ERROR_CREATED":
+            newState = { ...newState, errorPresent: true };
+            break;
         default:
+
             newState = { ...state }
             break;
     }
