707f7e3cf53fca3554245ed3e920a683480ccfc4
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-01 16:14:29 +0200

    created a drop down for all blocks.

--- storage-system-app/src/components/forms/insert-unit-form.js
+++ storage-system-app/src/components/forms/insert-unit-form.js
@@ -11,7 +11,12 @@ export class UnitForm extends Component {
                         <label htmlFor='unitName'>Unit Name</label>
                         <Field name='unitName' component="input" type="text" />
                         <label htmlFor='blockName'>Block Name</label>
-                        <Field name='blockName' component="input" type="text" />
+                        <Field name="favoriteColor" component="select">
+                            <option value="#ff0000">Select Block</option>
+                            {this.props.allBlocks.map(block => {
+                                return <option key={this.props.allBlocks.indexOf(block)} value={block.name}>{block.name}</option>
+                            })}
+                        </Field>
                     </div>
                 </div>
             </form>
