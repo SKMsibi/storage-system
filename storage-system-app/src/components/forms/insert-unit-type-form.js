13a7db1cc64b6763e725c69fbb647a136859c9e2
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-01 12:24:44 +0200

    created the insertUnitTypeForm component

---
+++ storage-system-app/src/components/forms/insert-unit-type-form.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
+import '../../App.css';
+
+class InsertUnitTypeForm extends Component {
+    render() {
+        return (
+            <div className="unit-type-form">
+                <form onSubmit={this.props.submitFunction}>
+                    <div className="form-row">
+                        <label htmlFor='typeName'>Name</label>
+                        <Field name='typeName' component="input" placeholder="example(Garage)" type="text" required={true} />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor='height'>Height</label>
+                        <Field name='height' component="input" min="1" placeholder="example(255)" type="number" required={true} />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor='length'>Length</label>
+                        <Field name='length' component="input" min="1" placeholder="example(255)" type="number" required={true} />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor='width'>Width</label>
+                        <Field name='width' component="input" min="1" placeholder="example(255)" type="number" required={true} />
+                    </div>
+                    <button>submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+InsertUnitTypeForm = reduxForm({
+    form: 'InsertUnitTypeForm'
+})(InsertUnitTypeForm)
+export default connect(null, null)(InsertUnitTypeForm);
