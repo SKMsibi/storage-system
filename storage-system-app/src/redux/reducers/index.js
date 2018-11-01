7b42eb6c848d907a877c4fc8ac21103951616d1b
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-01 12:27:09 +0200

    included the unitType reducer.

--- storage-system-app/src/redux/reducers/index.js
+++ storage-system-app/src/redux/reducers/index.js
@@ -1,12 +1,14 @@
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
 import business from './business';
-import location from './location'
-import blocks from './blocks'
-import { combineReducers } from 'redux'
+import location from './location';
+import blocks from './blocks';
+import unitType from './unit-types';
+import { combineReducers } from 'redux';
 
 export default combineReducers({
     form: formReducer,
     business: business,
     location: location,
+    unitType: unitType,
     blocks: blocks
 });
