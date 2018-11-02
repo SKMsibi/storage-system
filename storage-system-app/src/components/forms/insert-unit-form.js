dc1de079a8190ff4e0ef7adc5dca17eba8e92c2e
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-02 15:15:00 +0200

    reforctord the form component.

--- storage-system-app/src/components/forms/insert-unit-form.js
+++ storage-system-app/src/components/forms/insert-unit-form.js
@@ -5,20 +5,21 @@ import { Field, reduxForm } from 'redux-form'
 export class UnitForm extends Component {
     render() {
         return (
-            <form>
+            <form onSubmit={this.props.submitUnit}>
                 <div className="business-info">
                     <div className="form-row-unit">
                         <label htmlFor='unitName'>Unit Name</label>
-                        <Field name='unitName' component="input" type="text" />
+                        <Field name='unitName' component="input" type="text" required={true} />
                         <label htmlFor='blockName'>Block Name</label>
-                        <Field name="favoriteColor" component="select">
-                            <option value="#ff0000">Select Block</option>
+                        <Field name="blockName" component="select" required={true}>
+                            <option value="Select Block">Select Block</option>
                             {this.props.allBlocks.map(block => {
                                 return <option key={this.props.allBlocks.indexOf(block)} value={block.name}>{block.name}</option>
                             })}
                         </Field>
                     </div>
                 </div>
+                <button type="submit">submit</button>
             </form>
         )
     }
