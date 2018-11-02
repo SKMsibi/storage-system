4cd819c6c3ab31c9e0c8e5d26d816c24b7110d01
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-02 15:15:17 +0200

    added a feature to add a new unit.

--- storage-system-app/src/components/insert-unit.js
+++ storage-system-app/src/components/insert-unit.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { UnitForm } from "./forms/insert-unit-form";
-import { getAllUnitTypes, submitAUnitType, getBusinesses, getAllBlocks } from '../redux/thunks';
+import { getAllUnitTypes, submitAUnitType, getBusinesses, getAllBlocks, submitUnit } from '../redux/thunks';
 import InsertUnitTypeForm from './forms/insert-unit-type-form'
 import '../App.css';
+import * as actions from '../redux/actions'
 
 class InsertUnit extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            selectedBusiness: null,
             allTypes: [],
             showForm: false,
             showTypeForm: false,
@@ -19,6 +19,7 @@ class InsertUnit extends Component {
         this.addUnitType = this.addUnitType.bind(this);
         this.handleTypeSelect = this.handleTypeSelect.bind(this);
         this.handleBusinessSection = this.handleBusinessSection.bind(this);
+        this.submitUnit = this.submitUnit.bind(this);
     }
     componentDidMount() {
         this.props.getBusinesses();
@@ -28,26 +29,33 @@ class InsertUnit extends Component {
         this.setState({ showTypeForm: true, showForm: false });
     };
     async addUnitType(e) {
-        this.props.submitNewUnitType({ ...this.props.unitTypeValues.values, businessName: this.state.selectedBusiness })
+        this.props.submitNewUnitType({ ...this.props.unitTypeValues.values, businessName: this.props.selectedBusiness })
         this.setState({ showTypeForm: false });
         this.props.getUnitTypes();
         e.preventDefault()
     };
     handleBusinessSection(e) {
         if (e.target.value === "Select Business") {
-            this.setState({ showForm: false, showTypeForm: false })
+            this.setState({ showForm: false, showTypeForm: false });
+            this.props.changeSelectedBusiness(null)
         } else {
-            this.setState({ selectedBusiness: e.target.value })
+            this.props.changeSelectedBusiness(e.target.value)
         }
     };
     handleTypeSelect(e) {
         if (e.target.value === "select unit type") {
             this.setState({ showForm: false })
+            this.props.changeSelectedUnitType(null)
         } else {
-            this.setState({ selectedType: e.target.value, showForm: true, showTypeForm: false })
-            this.props.getAllBlocks(this.state.selectedBusiness)
+            this.setState({ showForm: true, showTypeForm: false })
+            this.props.changeSelectedUnitType(e.target.value)
+            this.props.getAllBlocks(this.props.selectedBusiness)
         }
     };
+    submitUnit(e) {
+        e.preventDefault()
+        this.props.submittingAUnit(this.props.unitValues.values, this.props.selectedUnitType, this.props.selectedBusiness);
+    }
     render() {
         return (
             <div className="App-container">
@@ -61,15 +69,15 @@ class InsertUnit extends Component {
                     </select>
                     <h4> Select the unit type of the unit or Add a new one.</h4>
                     <div>
-                        <select ref="unitType" onChange={this.handleTypeSelect}>
+                        <select ref="unitType" onChange={this.handleTypeSelect} disabled={!this.props.selectedBusiness}>
                             <option value="select unit type">Type name (height, length, width)</option>
                             {this.props.allUnitTypes.map(singleType => {
                                 return <option key={this.props.allUnitTypes.indexOf(singleType)} value={`${singleType.name},${singleType.height},${singleType.length},${singleType.width}`}>{singleType.name} ({singleType.height}, {singleType.length}, {singleType.width})</option>
                             })}
-                        </select><button onClick={this.showUnitTypeForm}>Add Type</button>
+                        </select><button onClick={this.showUnitTypeForm} disabled={!this.props.selectedBusiness}>Add Type</button>
                     </div>
                     {this.state.showForm && (
-                        <UnitForm allBlocks={this.props.allBlocks} />
+                        <UnitForm allBlocks={this.props.allBlocks} submitUnit={this.submitUnit} />
                     )}
                     {this.state.showTypeForm && (
                         <div>
@@ -88,7 +96,10 @@ const mapStateToProps = (state) => {
         allUnitTypes: state.unitType.allUnitTypes,
         unitTypeValues: state.form.InsertUnitTypeForm,
         businesses: state.business,
-        allBlocks: state.unitType.allBlocks
+        allBlocks: state.unit.allBlocks,
+        unitValues: state.form.InsertUnitForm,
+        selectedBusiness: state.business.selectedBusiness,
+        selectedUnitType: state.unit.selectedUnitType
     }
 }
 const mapDispatchToProps = (dispatch) => {
@@ -104,6 +115,15 @@ const mapDispatchToProps = (dispatch) => {
         },
         getAllBlocks: (businessName) => {
             dispatch(getAllBlocks(businessName))
+        },
+        changeSelectedBusiness: (business) => {
+            dispatch(actions.changeSelectedBusiness(business))
+        },
+        changeSelectedUnitType: (unitType) => {
+            dispatch(actions.setSelectedType(unitType))
+        },
+        submittingAUnit: (values, selectedUnitType, selectedBusiness) => {
+            dispatch(submitUnit(values, selectedUnitType, selectedBusiness))
         }
     }
 }
