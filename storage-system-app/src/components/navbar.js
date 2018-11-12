97228a5df8f021e30e705b9c01d0e4dfdb3734ce
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-12 10:39:07 +0200

    added a login route button.

--- storage-system-app/src/components/navbar.js
+++ storage-system-app/src/components/navbar.js
@@ -6,7 +6,8 @@ export default class navbar extends Component {
     render() {
         return (
             <header className="nav">
-                <Link to="/"><button className="navigation-button">Register Business</button></Link>
+                <Link to="/logIn"><button className="navigation-button">User Login</button></Link>
+                <Link to="/registerBusiness"><button className="navigation-button">Register Business</button></Link>
                 <Link to="/insertLocation"><button className="navigation-button">Business location</button></Link>
                 <Link to="/insertBlocks"><button className="navigation-button">Insert Blocks</button></Link>
                 <Link to="/insertUnitType"><button className="navigation-button">Insert unites</button></Link>
