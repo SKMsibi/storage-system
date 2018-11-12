5491a45967c3c5bcb40983692cc4b951a496c850
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-12 10:38:40 +0200

    moved the businessform into a component.

---
+++ storage-system-app/src/components/register-business.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import BusinessForm from './forms/register-business-form';
+export default class RegisterBusiness extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-container">
+                    <BusinessForm {...this.props} />
+                </div>
+            </div>
+        )
+    }
+}
