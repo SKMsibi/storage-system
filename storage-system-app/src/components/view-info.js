ec256e7349359ab25d0e7e21f1690d967872c1ef
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-15 16:20:14 +0200

    removed all axios code and replaced it with thunks.

--- storage-system-app/src/components/view-info.js
+++ storage-system-app/src/components/view-info.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import { connect } from 'react-redux';
+import * as thunks from '../redux/thunks';
 
-export default class viewInfo extends Component {
+
+class viewInfo extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            allUnitTypes: [],
-            allBusiness: [],
-            allLocations: [],
             showLocationDropDown: false,
             displaySelected: false,
             searchBy: '',
             searchPhrase: '',
             showInput: false,
-            units: [],
             displayUnits: false
         }
     }
@@ -26,14 +24,14 @@ export default class viewInfo extends Component {
         } else {
             this.setState({ displayUnits: false })
             if (this.refs.search.value === 'business') {
-                var allTheBusiness = await axios.get('http://localhost:3003/businesses');
-                this.setState({ allBusiness: allTheBusiness.data, showInput: true, searchBy: this.refs.search.value });
+                this.props.getAllBusinesses();
+                this.setState({ showInput: true, searchBy: this.refs.search.value });
             } else if (this.refs.search.value === "unit Types") {
-                var allUnitTypes = await axios.get('http://localhost:3003/unitTypes');
-                this.setState({ allUnitTypes: allUnitTypes.data, showInput: true, searchBy: this.refs.search.value });
+                this.props.getAllUnitTypes()
+                this.setState({ showInput: true, searchBy: this.refs.search.value });
             } else {
-                var allAvailableLocations = await axios.get('http://localhost:3003/allAvailableLocations');
-                this.setState({ showLocationDropDown: true, allLocations: allAvailableLocations.data, searchBy: this.refs.search.value })
+                this.props.getAllLocations()
+                this.setState({ showLocationDropDown: true, searchBy: this.refs.search.value })
             }
         }
     }
@@ -72,13 +70,11 @@ export default class viewInfo extends Component {
         this.refs.getButton.disabled = false;
     }
     async getData() {
-        var allUnits = await axios.get(`http://localhost:3003/allUnits/${this.state.searchBy}/${this.state.searchPhrase}`);
-        console.log('object :', allUnits.data);
-        if (allUnits) {
-            this.setState({ units: allUnits.data, displayUnits: true });
-        }
+        this.props.getAllAvailableUnits(this.state.searchBy, this.state.searchPhrase);
+        this.setState({ displayUnits: true });
     }
     render() {
+        console.log('this.pro :', this.props);
         return (
             <div className="App-container">
                 <h3>View all units</h3>
@@ -91,31 +87,57 @@ export default class viewInfo extends Component {
                 {this.state.showInput && this.state.searchBy === "business" && (
                     <select ref="select" name="business" onChange={() => this.handleChange()}>
                         <option value="Select Business">Select Business</option>
-                        {this.state.allBusiness.map(singleBusiness => {
-                            return <option key={this.state.allBusiness.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
+                        {this.props.businesses.map(singleBusiness => {
+                            return <option key={this.props.businesses.indexOf(singleBusiness)} value={singleBusiness.name}>{singleBusiness.name}</option>
                         })}
                     </select>)}
                 {this.state.showLocationDropDown && this.state.searchBy === "locations" && (
                     <select ref="locationDropDown" name="locationDropDown" onChange={() => this.handleLocationChange()}>
                         <option value="Select locations">Select locations</option>
-                        {this.state.allLocations.map(singleBusiness => {
-                            return <option key={this.state.allBusiness.indexOf(singleBusiness)} value={`${singleBusiness.region},${singleBusiness.city},${singleBusiness.address1},${singleBusiness.address2}`}>{singleBusiness.region}, {singleBusiness.city}, {singleBusiness.address1}, {singleBusiness.address2}</option>
+                        {this.props.locations.map(singleBusiness => {
+                            return <option key={this.props.locations.indexOf(singleBusiness)} value={`${singleBusiness.region},${singleBusiness.city},${singleBusiness.address1},${singleBusiness.address2}`}>{singleBusiness.region}, {singleBusiness.city}, {singleBusiness.address1}, {singleBusiness.address2}</option>
                         })}
                     </select>
                 )}
                 {this.state.showInput && this.state.searchBy === "unit Types" && (
                     <select ref="select" name="unit types" onChange={() => this.handleChange()}>
                         <option value="Select unit type">Select unit type (height, length, width)</option>
-                        {this.state.allUnitTypes.map(singleUnitType => {
-                            return <option key={this.state.allUnitTypes.indexOf(singleUnitType)} value={`${singleUnitType.name}, ${singleUnitType.height}, ${singleUnitType.length}, ${singleUnitType.width}`}> {singleUnitType.name} ({singleUnitType.height}, {singleUnitType.length}, {singleUnitType.width})</option>
+                        {this.props.unitTypes.map(singleUnitType => {
+                            return <option key={this.props.unitTypes.indexOf(singleUnitType)} value={`${singleUnitType.name}, ${singleUnitType.height}, ${singleUnitType.length}, ${singleUnitType.width}`}> {singleUnitType.name} ({singleUnitType.height}, {singleUnitType.length}, {singleUnitType.width})</option>
                         })}
                     </select>)}
                 <button onClick={() => this.getData()} ref="getButton">Go</button>
                 {this.state.displayUnits && (
-                    this.state.units.length <= 0 ? <p>Sorry No units are available for the selected options.</p> : this.state.units.map(item =>
-                        <h4 key={this.state.units.indexOf(item)}>{item.name}</h4>)
+                    this.props.units.length <= 0 ? <p>Sorry No units are available for the selected options.</p> : this.props.units.map(item =>
+                        <h4 key={this.props.units.indexOf(item)}>{item.name}</h4>)
                 )}
             </div >
         )
     }
 }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getAllBusinesses: () => {
+            dispatch(thunks.getBusinessesForUnits())
+        },
+        getAllUnitTypes: () => {
+            dispatch(thunks.getAllUnitTypesForUnis())
+        },
+        getAllLocations: () => {
+            dispatch(thunks.getLocationsForUnits())
+        },
+        getAllAvailableUnits: (searchBy, searchPhrase) => {
+            dispatch(thunks.getAllAvailableUnits(searchBy, searchPhrase))
+        }
+    }
+}
+const mapStateToProps = (state) => {
+    return {
+        state: state.displayUnits,
+        businesses: state.displayUnits.allBusiness,
+        unitTypes: state.displayUnits.allUnitTypes,
+        locations: state.displayUnits.allLocations,
+        units: state.displayUnits.allUnits
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(viewInfo);
