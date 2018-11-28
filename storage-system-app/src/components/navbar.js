3fc1cff801915aa47007f83418c2c5cae5c50788
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-11-28 10:37:45 +0200

    inserted the log out button.

--- storage-system-app/src/components/navbar.js
+++ storage-system-app/src/components/navbar.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 export default class navbar extends Component {
     changeRoute(route) {
+        if (route === "/") {
+            sessionStorage.removeItem('jwtToken');
+        }
         this.props.history.push(route)
     }
     render() {
         return (
             <header className="nav">
+                <button onClick={() => this.changeRoute("/")} className="navigation-button">User log out</button>
+                <button onClick={() => this.changeRoute("/signUp")} className="navigation-button">User sign-up</button>
                 <button onClick={() => this.changeRoute("/logIn")} className="navigation-button">User Login</button>
                 <button onClick={() => this.changeRoute("/registerBusiness")} className="navigation-button">Register Business</button>
                 <button onClick={() => this.changeRoute("/insertLocation")} className="navigation-button">Business location</button>
                 <button onClick={() => this.changeRoute("/insertBlocks")} className="navigation-button">Insert Blocks</button>
                 <button onClick={() => this.changeRoute("/insertUnitType")} className="navigation-button">Insert unites</button>
                 <button onClick={() => this.changeRoute("/displayUnits")} className="navigation-button">view Units</button>
-            </header>
+            </header >
         )
     }
 }
