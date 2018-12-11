0d16d6e9454cda0553864241c260e56077d299df
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-12-11 15:20:07 +0200

    added a feature where a user can place a order.

--- storage-system-app/src/components/view-units-table.js
+++ storage-system-app/src/components/view-units-table.js
@@ -1,42 +1,49 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { PlaceOrder } from '../redux/thunks'
 import "../App.css"
 
 class ViewUnitsTable extends Component {
+    placeOrder(details) {
+        this.props.placeOrder(details);
+    }
     render() {
-        console.log('this.props :', this.props);
         return (
             <div>
-                <table key="unitsTable">
-                    <thead>
-                        <tr>
-                            <th>No.</th>
-                            <th>Unit Name</th>
-                            <th>Block name</th>
-                            <th>Unit type</th>
-                            <th>length</th>
-                            <th>width</th>
-                            <th>height</th>
-                            <th>Region</th>
-                            <th>City</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.units.length <= 0 ? <tr><td>Sorry No units are available for the selected options.</td></tr> : this.props.units.map(singleUnit =>
-                            <tr key={this.props.units.indexOf(singleUnit)}>
-                                <td>{this.props.units.indexOf(singleUnit) + 1}</td>
-                                <td>{singleUnit.unitsname}</td>
-                                <td>{singleUnit.blockname}</td>
-                                <td>{singleUnit.unittypename}</td>
-                                <td>{singleUnit.length}</td>
-                                <td>{singleUnit.width}</td>
-                                <td>{singleUnit.height}</td>
-                                <td>{singleUnit.region}</td>
-                                <td>{singleUnit.city}</td>
+                {this.props.units.length <= 0 ? <h4>Sorry No units are available for the selected options.</h4> :
+                    <table key="unitsTable">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Unit Name</th>
+                                <th>Block name</th>
+                                <th>Unit type</th>
+                                <th>length</th>
+                                <th>width</th>
+                                <th>height</th>
+                                <th>Region</th>
+                                <th>City</th>
+                                <th></th>
                             </tr>
-                        )}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {this.props.units.map(singleUnit =>
+                                <tr key={this.props.units.indexOf(singleUnit)}>
+                                    <td>{this.props.units.indexOf(singleUnit) + 1}</td>
+                                    <td>{singleUnit.unitsname}</td>
+                                    <td>{singleUnit.blockname}</td>
+                                    <td>{singleUnit.unittypename}</td>
+                                    <td>{singleUnit.length}</td>
+                                    <td>{singleUnit.width}</td>
+                                    <td>{singleUnit.height}</td>
+                                    <td>{singleUnit.region}</td>
+                                    <td>{singleUnit.city}</td>
+                                    <td><button onClick={() => this.placeOrder(singleUnit)}>Order</button></td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                }
             </div>
         )
     }
@@ -46,4 +53,11 @@ const mapStateToProps = (state) => {
         units: state.displayUnits.allUnits
     }
 }
-export default connect(mapStateToProps, null)(ViewUnitsTable);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        placeOrder: (unitDetails) => {
+            dispatch(PlaceOrder(unitDetails))
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ViewUnitsTable);
