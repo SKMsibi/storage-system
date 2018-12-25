5cbf576d7926bf8e88910e4bef3bf0157ebe19bf
SKMsibi <sabelokabelomsibiskm@gmail.com>
2018-12-25 15:49:53 +0200

    changed navbar to use react-bootstrap elements.

--- storage-system-app/src/components/navbar.js
+++ storage-system-app/src/components/navbar.js
@@ -1,27 +1,73 @@
-import React, { Component } from 'react'
-export default class navbar extends Component {
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+
+class navbar extends Component {
     changeRoute(route) {
         if (route === "/logOut") {
             sessionStorage.removeItem("jwtToken");
-            this.props.history.push("/logIn")
+            this.props.history.push("/logIn");
         } else {
-            this.props.history.push(route)
+            this.props.history.push(route);
         }
     }
     render() {
         return (
-            <header className="nav">
-                <button onClick={() => this.changeRoute("/logOut")} className="navigation-button">User log out</button>
-                <button onClick={() => this.changeRoute("/signUp")} className="navigation-button">User sign-up</button>
-                <button onClick={() => this.changeRoute("/logIn")} className="navigation-button">User Login</button>
-                <button onClick={() => this.changeRoute("/registerBusiness")} className="navigation-button">Register Business</button>
-                <button onClick={() => this.changeRoute("/insertLocation")} className="navigation-button">Business location</button>
-                <button onClick={() => this.changeRoute("/insertBlocks")} className="navigation-button">Insert Blocks</button>
-                <button onClick={() => this.changeRoute("/insertUnitType")} className="navigation-button">Insert unites</button>
-                <button onClick={() => this.changeRoute("/displayUnits")} className="navigation-button">View All Units</button>
-                <button onClick={() => this.changeRoute("/displayUserUnits")} className="navigation-button">View My Units</button>
-                <button onClick={() => this.changeRoute("/rentedOutUnits")} className="navigation-button">Rented Units</button>
-            </header >
+            <div>
+                <Navbar fluid className={'navbar-bg'}>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a>StorageGuru</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            {this.props.userStatus && (
+                                <NavItem onClick={() => this.changeRoute("/logOut")} >User log out</NavItem>
+                            )}
+                        </Nav>
+                        {!this.props.userStatus && (
+                            <Nav pullRight>
+                                <NavItem eventKey={1} onClick={() => this.changeRoute("/signUp")}>User sign-up</NavItem>
+                                <NavItem eventKey={2} onClick={() => this.changeRoute("/logIn")}>User Login</NavItem>
+                            </Nav>
+                        )}
+                        {(this.props.userStatus && this.props.role === "Storage Owner") && (
+                            <Nav pullRight>
+                                <NavItem onClick={() => this.changeRoute("/registerBusiness")} >Register Business</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/insertLocation")} >Business location</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/insertBlocks")} >Insert Blocks</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/insertUnitType")} >Insert unites</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/rentedOutUnits")} >Rented Units</NavItem>
+                            </Nav>
+                        )}
+                        {(this.props.userStatus && this.props.role === "Storage Owner") && (
+                            <Nav pullRight>
+                                <NavItem onClick={() => this.changeRoute("/registerBusiness")} >Register Business</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/insertLocation")} >Business location</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/insertBlocks")} >Insert Blocks</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/insertUnitType")} >Insert unites</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/rentedOutUnits")} >Rented Units</NavItem>
+                            </Nav>
+                        )}
+                        {(this.props.userStatus && this.props.role === "Storage Ranter") && (
+                            <Nav pullRight>
+                                <NavItem onClick={() => this.changeRoute("/displayUnits")} >View All Units</NavItem>
+                                <NavItem onClick={() => this.changeRoute("/displayUserUnits")} >View My Units</NavItem>
+                            </Nav>
+                        )}
+                    </Navbar.Collapse>
+                </Navbar>
+            </div >
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        state: state,
+        userStatus: state.SignInLogIn.userLoggedIn,
+        role: state.SignInLogIn.loggedInUserRole
+    }
+}
+export default connect(mapStateToProps, null)(navbar)
