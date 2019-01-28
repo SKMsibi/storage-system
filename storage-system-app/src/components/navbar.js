cd0d33aeb60d53a50a11704f9dfed691e4671bb8
SKMsibi <sabelokabelomsibiskm@gmail.com>
2019-01-28 11:37:43 +0200

    removed dublicates.

--- storage-system-app/src/components/navbar.js
+++ storage-system-app/src/components/navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { updateUserStatus } from '../redux/thunks'
 class navbar extends Component {
     changeRoute(route) {
         if (route === "/logOut") {
@@ -22,51 +22,45 @@ class navbar extends Component {
                 </div>
                 <div className="nav-links">
                     {this.props.userStatus && (
-                        <a onClick={() => this.changeRoute("/logOut")} >log out</a>
+                        // eslint-disable-next-line
+                        <a onClick={() => { this.changeRoute("/logOut"); this.props.logOut(); return true; }} >log out</a>
                     )}
                     {!this.props.userStatus && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/signUp")}>Sign-up</a>
                     )}
                     {!this.props.userStatus && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/logIn")}>Login</a>
                     )}
 
                     {(this.props.userStatus && this.props.role === "Storage Owner") && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/registerBusiness")} >Register Business</a>
                     )}
                     {(this.props.userStatus && this.props.role === "Storage Owner") && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/insertLocation")} >Business location</a>
                     )}
                     {(this.props.userStatus && this.props.role === "Storage Owner") && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/insertBlocks")} >Insert Blocks</a>
                     )}
                     {(this.props.userStatus && this.props.role === "Storage Owner") && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/insertUnitType")} >Insert unites</a>
                     )}
                     {(this.props.userStatus && this.props.role === "Storage Owner") && (
-                        <a onClick={() => this.changeRoute("/rentedOutUnits")} >Rented Units</a>
-                    )}
-
-                    {(this.props.userStatus && this.props.role === "Storage Owner") && (
-                        <a onClick={() => this.changeRoute("/registerBusiness")} >Register Business</a>
-                    )}
-                    {(this.props.userStatus && this.props.role === "Storage Owner") && (
-                        <a onClick={() => this.changeRoute("/insertLocation")} >Business location</a>
-                    )}
-                    {(this.props.userStatus && this.props.role === "Storage Owner") && (
-                        <a onClick={() => this.changeRoute("/insertBlocks")} >Insert Blocks</a>
-                    )}
-                    {(this.props.userStatus && this.props.role === "Storage Owner") && (
-                        <a onClick={() => this.changeRoute("/insertUnitType")} >Insert unites</a>
-                    )}
-                    {(this.props.userStatus && this.props.role === "Storage Owner") && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/rentedOutUnits")} >Rented Units</a>
                     )}
 
                     {(this.props.userStatus && this.props.role === "Storage Ranter") && (
-                        <a onClick={() => this.changeRoute("/displayUnits")} >View All Units</a>
+                        // eslint-disable-next-line
+                        <a onClick={() => this.changeRoute("/displayUnits")} >Rent a unit</a>
                     )}
                     {(this.props.userStatus && this.props.role === "Storage Ranter") && (
+                        // eslint-disable-next-line
                         <a onClick={() => this.changeRoute("/displayUserUnits")} >View My Units</a>
                     )}
                 </div>
@@ -82,4 +76,11 @@ const mapStateToProps = (state) => {
         role: state.SignInLogIn.loggedInUserRole
     }
 }
-export default connect(mapStateToProps, null)(navbar)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        logOut: () => {
+            dispatch(updateUserStatus(false, null))
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(navbar)
